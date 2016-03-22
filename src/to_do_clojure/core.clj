2ae4f4bf8c8d28a283f4c915d7528911524be10b
Alex <amhughes@g.cofc.edu>
2016-03-22 11:14:00 -0400

    Initial import

---
+++ src/to_do_clojure/core.clj
@@ -0,0 +1,19 @@
+(ns to-do-clojure.core
+  (:gen-class))
+
+(def todos (atom [])); holds the todos
+
+
+(defn -main []
+  (println "Type a to-do and hit enter. Type q to quit.")
+  (loop []
+    (let [text (read-line)];read-line function
+      (when (not= text "q")
+        (reset! todos (conj @todos text))
+        (recur))))
+  (println @ todos))
+        
+         
+    
+    
+    
